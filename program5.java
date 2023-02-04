a38273c71065b256f7a7d47b5589667ad7f5258a
Jayavardhan-swarna <79640790+Jayavardhan-swarna@users.noreply.github.com>
2023-02-04 19:49:57 +0530

    Create program5.java

---
+++ program5.java
@@ -0,0 +1,16 @@
+//12002055 Jaya Vardhan Swarna
+class A{
+    public void methodA(){
+        System.out.println("A");
+    }
+}
+class B extends A{
+    public void methodB() {
+        System.out.println("B");
+    }
+}
+class C extends B{
+    public void methodC() {
+        System.out.println("C");
+    }
+}
