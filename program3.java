9eaee36891cf8e71ba9bf3b18b18782caad37e54
Jayavardhan-swarna <79640790+Jayavardhan-swarna@users.noreply.github.com>
2023-02-04 19:48:49 +0530

    Create program3.java

---
+++ program3.java
@@ -0,0 +1,17 @@
+//12002055 Jaya Vardhan Swarna
+class Father {
+    public final String lastName="Oberoi";
+    public void foodHabit() {
+        System.out.println("I like Salty");
+    }
+}
+class Son extends Father {
+    public void foodHabit() {
+        System.out.println("I like sweet");
+    }
+}
+public class program3 {
+     public static void main(String[] args) {
+        
+     }
+}
